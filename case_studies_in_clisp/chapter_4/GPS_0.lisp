9d0fd2981776172427560d9c9b576bc737c9b114
imsoda <loveme.tjs@gmail.com>
2013-04-21 22:54:53 +0800

    Implemented GPS version 2.

--- case_studies_in_clisp/chapter_4/GPS_0.lisp
+++ case_studies_in_clisp/chapter_4/GPS_0.lisp
@@ -6,6 +6,31 @@
       (apply #'remove item sequence
 	     :test (complement test) keyword-args)))
 
+(defvar *dbg-ids* nil "Identifiers used by dbg.")
+
+(defun dbg (id format-string &rest args)
+  "Print debugging info if (DEBUG ID) has been specified."
+  (when (member id *dbg-ids*)
+    (fresh-line *debug-io*)
+    (apply #'format *debug-io* format-string args)))
+
+(defun start-debug (&rest ids)
+  "Start dbg output on the given ids."
+  (setf *dbg-ids* (union ids *dbg-ids*)))
+
+(defun stop-debug (&rest ids)
+  "Stop dbg on the ids. With no ids, stop dbg altogether."
+  (setf *dbg-ids* (if (null ids)
+		      nil
+		      (set-difference *dbg-ids* ids))))
+
+(defun dbg-indent (id indent format-string &rest args)
+  "Print indented debugging info if (DEBUG ID) has been specified."
+  (when (member id *dbg-ids*)
+    (fresh-line *debug-io*)
+    (dotimes (i indent) (princ " " *debug-io*))
+    (apply #'format *debug-io* format-string args)))
+
 (defvar *state* nil "The current state: a list of conditions.")
 
 (defvar *ops* nil "A list of available operators.")
@@ -17,25 +42,71 @@
   (add-list nil)
   (del-list nil))
 
-(defun GPS (*state* goals *ops*)
+(defun GPS (state goals &optional (*ops* *ops*))
   "General Problem Solver: achieve all goals using *op*."
-  (if (every #'achieve goals) 'solved))
+  (remove-if #'atom (achieve-all (cons '(start) state) goals nil)))
 
-(defun achieve (goal)
+(defun achieve-all (state goals goal-stack)
+  "Achieve each goal and make sure they still hold at the end."
+  (let ((current-state state))
+    (if (and (every #'(lambda (g)
+			(setf current-state
+			      (achieve current-state g goal-stack)))
+		    goals)
+	     (subsetp goals current-state :test #'equal))
+	current-state)))
+
+(defun achieve (state goal goal-stack)
   "A goal is achieved if it already holds.
    Or if there is an appropriate op for it that is applicable."
-  (or (member goal *state*)
-      (some #'apply-op
-	    (find-all goal *ops* :test #'appropriate-p))))
+  (dbg-indent :gps (length goal-stack) "Goal:~a" goal)
+  (cond ((member-equal goal state) state)
+	((member-equal goal goal-stack) nil)
+	(t (some #'(lambda (op) (apply-op state goal op goal-stack))
+		 (find-all goal *ops* :test #'appropriate-p)))))
+
+(defun member-equal (item list)
+  (member item list :test #'equal))
 
 (defun appropriate-p (goal op)
   "An op is appropriate to a goal if it is in its add list."
-  (member goal (op-add-list op)))
+  (member-equal goal (op-add-list op)))
 
-(defun apply-op (op)
-  "Print a message and update *state* if op is applicable."
-  (when (every #'achieve (op-preconds op))
-    (print (list 'executing (op-action op)))
-    (setf *state* (set-difference *state* (op-del-list op)))
-    (setf *state* (union *state* (op-add-list op)))
-    t))
+(defun apply-op (state goal op goal-stack)
+  "Return a new, transformed state if op is applicable."
+  (dbg-indent :gps (length goal-stack) "Consider: ~a" (op-action op))
+  (let ((state2 (achieve-all state (op-preconds op)
+			     (cons goal goal-stack))))
+    (unless (null state2)
+      ;; Return an updated state
+      (dbg-indent :gps (length goal-stack) "Action: ~a" (op-action op))
+      (append (remove-if #'(lambda (x)
+			     (member-equal x (op-del-list op)))
+			 state2)
+	      (op-add-list op)))))
+
+(defun use (oplist)
+  "Use oplist as the default list of operators."
+  ;;Return something useful, but not too verbose:
+  ;; the number of operators.
+  (length (setf *ops* oplist)))
+
+(defun executing-p (x)
+  "Is x of the form: (executing ...) ?"
+  (starts-with x 'executing))
+
+(defun starts-with (list x)
+  "Is this a list whose first element is x?"
+  (and (consp list) (eql (first list) x)))
+
+(defun convert-op (op)
+  "Make op conform to the (EXECUTING op) convention."
+  (unless (some #'executing-p (op-add-list op))
+    (push (list 'executing (op-action op)) (op-add-list op)))
+  op)
+
+(defun op (action &key preconds add-list del-list)
+  "Make a new operator that obeys the (EXECUTING op) convention."
+  (convert-op
+   (make-op :action action :preconds preconds
+	    :add-list add-list :del-list del-list)))
